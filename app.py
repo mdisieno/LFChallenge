7b1631cf9d2f84d3e45ada7873ae091614c607e2
Michael Disieno <mdisieno@gmail.com>
2023-08-27 15:06:05 -0700

    alphabetically sorted by last then first name. I'm actually having a lot of fun learning this approach. :)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,jsonify
 from urllib.request import urlopen
-import json
+from operator import itemgetter
+# import json
 import requests
 
 
@@ -17,25 +18,21 @@ def get_supervisors():
   #https://bobbyhadz.com/blog/python-typeerror-the-json-object-must-be-str-bytes-or-bytearray-not-dict#the-json-object-must-be-str-bytes-or-bytearray-not-response000
   response = requests.get("https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers")
   parsed_data = response.json()
-  outputDictionary = {}
-  #Go through each value in parsed_data
-  for x in parsed_data:
-    activeLine = (parsed_data[x])
-    jurisdiction = activeLine['id']
-    lastName = activeLine['lastName']
-    firstName = activeLine['firstName'] 
+  outputList = []
+  print(type(parsed_data))  
+  for activeLine in parsed_data:
+    jurisdiction = activeLine['jurisdiction']
+    #Numeric jurisdictions should be excluded from the response
+    if not jurisdiction.isdigit():
+      lastName = activeLine['lastName']
+      firstName = activeLine['firstName'] 
+      entry = [jurisdiction, activeLine['lastName'], activeLine['firstName']]
+      # https://sparkbyexamples.com/python/create-list-of-lists-in-python/#:~:text=To%20create%20a%20list%20of%20lists%20in%20Python%2C%20you%20can,pop()%20%2C%20and%20del%20statement.
+      outputList.append(entry)
 
-  return(lastIndex)
-  # return(parsed_data)
-
-
-  # response = urlopen("https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers")
-  # decoded_reponse = response.read().decode(response)
-  # supervisors = json.loads(decoded_response)
-  # # id = supervisors[1]
-  # print(supervisors)
-  # return()
-
+  #https://sparkbyexamples.com/python/python-sort-list-of-lists/
+  outputList = sorted(outputList, key=itemgetter(1, 2))
+  return(outputList)
 
 
 @app.route("/api/postSupervisors", methods=['POST'])
