71849bac74d9b3a42af4ee9549f3b91c4b8cdee5
Michael Disieno <mdisieno@gmail.com>
2023-08-27 19:01:23 -0700

    post done, checks set. Cleaning up and checking that I hit all the bullets

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,url_for,redirect,render_template
 from urllib.request import urlopen
 from operator import itemgetter
 # import json
@@ -10,39 +10,41 @@ app = Flask(__name__)
 
 @app.route("/")
 def sayHello():
-  return "Hello! I'm Mike. To get the supervisors, please use /getSupervisors. To post, please use /postSupervisor"
+  return "Hello! I'm Mike. To get the supervisors, please use /api/getSupervisors. To post, please use /api/postSupervisor"
 
 
 @app.route("/api/getSupervisors", methods=['GET'])
 def get_supervisors():
-  response = requests.get("https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers")
+  response = requests.get("https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers") # Pull down list of managers
   parsed_data = response.json()
-  outputList = []
   sortedList = []
+  outputList = []
   #Pull relevant information from JSON
   for activeLine in parsed_data:
     jurisdiction = activeLine['jurisdiction']
-    #Numeric jurisdictions should be excluded from the response
-    if not jurisdiction.isdigit():
+    if not jurisdiction.isdigit(): #numeric jurisdictions should be excluded from the response
       entry = [jurisdiction, activeLine['lastName'], activeLine['firstName']]
       sortedList.append(entry)
   #Sort Output
-  sortedList = sorted(sortedList, key=itemgetter(1, 2))
+  sortedList = sorted(sortedList, key=itemgetter(0, 1, 2)) # Sorted in alphabetic order by jurisdiction, lastName, firstName
   for activeLine in sortedList:
-    outputList.append(activeLine[0] + " - " + activeLine[1] + ", " + activeLine[2])
+    outputList.append(activeLine[0] + " - " + activeLine[1] + ", " + activeLine[2]) # "jurisdiction = lastName, firstName"
   return(outputList)
 
 
-@app.route("/api/postSupervisors", methods=['POST'])
+@app.route("/api/postSupervisors", methods=['POST','GET'])
 def post_supervisors():
-  data = request.get_json()
-  firstName = ""
-  LastName = ""
-  email = ""
-  phoneNumber = ""
-  supervisor = ""
-  return()
-
+  if request.method == "POST":
+      firstName = request.form['firstName'] #required
+      lastName  = request.form['lastName'] #required
+      email = request.form['email']
+      phoneNumber  = request.form['phoneNumber']
+      supervisor = request.form['supervisor'] #required
+      output = firstName + " " + lastName + " " + email + " " + phoneNumber + " " + supervisor
+      print(output) #Printed to the console upon receiving the post request
+      return(output)
+  else:
+    return render_template("postSupervisors.html")
 
 if __name__ == "__main__":
   app.run()
